ddf8e41d368caa432fe3fd7cfb25c5a73c3d725d
iaugusty <iaugusty@its.jnj.com>
2025-09-08 21:31:59 +0000

    fix sas rounding when result is 0 and input is negative

--- R/format_value.R
+++ R/format_value.R
@@ -241,7 +241,8 @@ round_sas <- function(x,
   z <- z + 0.5 + sqrt(.Machine$double.eps)
   z <- trunc(z)
   z <- z / 10^digits
-  z <- z * posneg
+  # only include sign when rounded value strictly negative
+  if (z < 0) z <- z * posneg
   ## return numeric vector of rounded values
   z
 }
